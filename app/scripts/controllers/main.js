f862fc46d6fd04d6e1f8fb5694ce196d671599bd
Matthew Goo <mattgoo@gmail.com>
2013-11-08 15:24:47 -0800

    alerts working

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,9 +6,11 @@ angular.module('tictactoeAngularApp')
 
     $scope.board = board.create(size);
     $scope.player = true;  // true = p1, false = p2
+    var moves = 0;
 
     $scope.placePiece = function(row, col) {
       if($scope.board[row][col].value === undefined) {
+        moves++;
         if($scope.player) {
           $scope.board[row][col].value = 'X';
           $scope.board[row][col].points = 1;
@@ -17,7 +19,14 @@ angular.module('tictactoeAngularApp')
           $scope.board[row][col].points = 10;
         }
         $scope.player = !$scope.player;
-        board.checkWinner($scope.board);
+        console.log(board.checkWinner($scope.board));
+        if(board.checkWinner($scope.board) === 'p1Win') {
+          $scope.gamewinP1 = true;
+        } else if(board.checkWinner($scope.board) === 'p2Win') {
+          $scope.gamewinP2 = true;
+        } else if(moves === ($scope.board.length*$scope.board.length-1)) {
+          $scope.gamelost = true;
+        }
       }
     };
   });
